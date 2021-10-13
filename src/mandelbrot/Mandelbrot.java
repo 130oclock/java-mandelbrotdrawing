80fb39fc7521133940184eeda3f72ac86e8f06a9
130oclock <79950921+130oclock@users.noreply.github.com>
2021-10-13 09:12:08 -0400

    Create Mandelbrot.java

---
+++ src/mandelbrot/Mandelbrot.java
@@ -0,0 +1,12 @@
+package mandelbrot;
+
+class Mandelbrot {
+	
+	public int[] screenRoster;
+	
+	public Mandelbrot(int screenWidth, int screenHeight, int x0, int x1, int y0, int y1) {
+		screenRoster = new int[screenWidth * screenHeight];
+	}
+	
+	
+}
