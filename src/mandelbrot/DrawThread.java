96da4ff19d40e8d4e97707a9d074370a6faf200d
130oclock <79950921+130oclock@users.noreply.github.com>
2021-10-15 10:55:23 -0400

    Update DrawThread.java

--- src/mandelbrot/DrawThread.java
+++ src/mandelbrot/DrawThread.java
@@ -10,17 +10,22 @@ class DrawThread implements Runnable {
 	private int screenHalfWidth, screenHalfHeight, screenLength, max;
 	private double total;
 	
-	private int x0, x1, y0, y1;
+	private double[] patches;
+	private boolean[] threadPatches;
+	private Graphics graphic;
 	
-	public DrawThread(int x0, int x1, int y0, int y1) {
-		this.x0 = x0;
-		this.y0 = y0;
-		this.x1 = x1;
-		this.y1 = y1;
+	public DrawThread(double[] patches, boolean[] threadPatches. Graphics g) {
+		this.patches = patches;
+		this.threadPatches = threadPatches;
+		this.graphic = g;
 	}
 	
 	@Override
 	public void run() {
+		
+	}
+	
+	private void draw(Graphics g) {
 		
 	}
 }
